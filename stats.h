62b9129b6fae245775fcabd7388ca5287b99e3ca
Dave White <davewhite.pv@gmail.com>
2017-04-21 10:24:33 -0600

    Creating Initial Coursera Repository

---
+++ stats.h
@@ -0,0 +1,41 @@
+/******************************************************************************
+ * Copyright (C) 2017 by Alex Fosdick - University of Colorado
+ *
+ * Redistribution, modification or use of this software in source or binary
+ * forms is permitted as long as the files maintain this copyright. Users are 
+ * permitted to modify this and use it to learn about the field of embedded
+ * software. Alex Fosdick and the University of Colorado are not liable for any
+ * misuse of this material. 
+ *
+ *****************************************************************************/
+/**
+ * @file  stats.h 
+ * @brief stats.c header file for assessment m1
+ *
+ * header file for stats.c. this file hosts the declarations for functions that 
+ * run in the stats.c file.
+ *
+ * @author  David White
+ * @date 04-21-2017
+ *
+ */
+#ifndef __STATS_H__
+#define __STATS_H__
+
+/* Add Your Declarations and Function Comments here */ 
+
+/**
+ * @brief <Add Brief Description of Function Here>
+ *
+ * <Add Extended Description Here>
+ *
+ * @param <Add InputName> <add description here>
+ * @param <Add InputName> <add description here>
+ * @param <Add InputName> <add description here>
+ * @param <Add InputName> <add description here>
+ *
+ * @return <Add Return Informaiton here>
+ */
+
+
+#endif /* __STATS_H__ */
